f42bcbd62345eeaf292f44a2679c33ed5ace0920
Jason Conny <jasonconny@gmail.com>
2020-08-20 00:21:56 -0500

    removing resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const resolve = require('resolve');
 const postcssNormalize = require('postcss-normalize');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
