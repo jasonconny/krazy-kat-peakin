c22023f7bc6e2c9316df27af3a78725e27cb6230
Jason Conny <jasonconny@gmail.com>
2021-05-24 17:10:14 -0500

    stub out Releases list

--- src/views/ReleasesView/ReleasesView.tsx
+++ src/views/ReleasesView/ReleasesView.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { ArtistContext } from 'providers/ArtistProvider';
+import { ReleasesContext } from 'providers/ReleasesProvider';
 import { PrimaryLayout } from 'components/layouts';
+import List from 'components/List';
 
 const ReleasesView: React.FC = () => {
     const artist = React.useContext(ArtistContext);
+    const releases = React.useContext(ReleasesContext);
 
     return (
         <PrimaryLayout>
@@ -11,6 +14,19 @@ const ReleasesView: React.FC = () => {
                 <h2>{artist?.name}</h2>
 
                 <h3>Releases</h3>
+
+                {releases && releases.releases.length > 0 ? (
+                    <List>
+                        {releases.releases
+                            .filter(release => !!release)
+                            .map(release => (
+                                <li key={release.id}>
+                                    {release.title}
+                                </li>
+                            ))
+                        }
+                    </List>
+                ) : null}
             </section>
         </PrimaryLayout>
     );
