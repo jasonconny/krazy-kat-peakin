9c6ba603e61684df8ac9b3a2dbac933bb20386e5
Jason Conny <jasonconny@gmail.com>
2022-02-20 10:16:32 -0600

    revert HomeView; format eslintrc

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { ArtistContext } from 'providers/ArtistProvider';
-import GenericComponent from 'components/GenericComponent'
 
 const HomeView: React.FC = () => {
     const artist = React.useContext(ArtistContext);
@@ -12,8 +11,6 @@ const HomeView: React.FC = () => {
             <h3>Profile</h3>
 
             <p>{artist?.profile}</p>
-
-            <GenericComponent excited={true} fooProp={'Aloha Mr. Hand'} />
         </section>
     );
 };
